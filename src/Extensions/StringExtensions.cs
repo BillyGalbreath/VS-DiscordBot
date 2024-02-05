c817a65053885b61a040efae6edcfa394d2b2939
William Blake Galbreath <Blake.Galbreath@GMail.com>
2024-02-05 14:37:53 -0600

    move plural formatter

--- src/Extensions/StringExtensions.cs
+++ src/Extensions/StringExtensions.cs
@@ -1,4 +1,4 @@
-﻿using DiscordBot.Util;
+﻿using System;
 
 namespace DiscordBot.Extensions;
 
@@ -7,5 +7,24 @@ public static class StringExtensions {
 
     public static string Format(this string format, params object?[] args) {
         return string.Format(PluralFormatProvider, format, args);
+    }
+}
+
+public class PluralFormatProvider : IFormatProvider, ICustomFormatter {
+    public object GetFormat(Type? formatType) {
+        return this;
+    }
+
+    public string Format(string? format, object? arg, IFormatProvider? formatProvider) {
+        if (format == null) {
+            return $"{arg}";
+        }
+
+        if (!format.Contains(';')) {
+            return string.Format($"{{0:{format}}}", arg);
+        }
+
+        int i = (int)(arg ?? 0) == 1 ? 0 : 1;
+        return $"{arg} {format.Split(';')[i]}";
     }
 }
